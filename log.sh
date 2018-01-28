028bfd99fef1b06352d0306991ea55f17661dab4
vikbert <xun.zhou@momox.biz>
2018-01-29 00:33:32 +0100

    removed non-used files

--- log.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ -z "$1" ]; then
-  tail -f /var/log/nginx/*error.log /srv/www/shop/app/logs/shop/applog.log
-else
-  tail -n "$1" /var/log/nginx/*error.log /srv/www/shop/app/logs/shop/applog.log
-fi
