b097206ced2c390c4043f24373eee95f8ac6f890
Xun Zhou <segentor@gmail.com>
2019-09-26 21:45:04 +0200

    update fish aliases

--- fish/install.sh
+++ fish/install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 ln -s ./config.fish ~/.config/fish/config.fish
-echo "create soft-link for command aliases. \n"
+echo "create soft-link for command aliases."
 
 ln -s ./fish_variables  ~/.config/fish/fish_variables
-echo "create soft-link for fish shell variable(this can define the color of fish shell prompt). \n"
+echo "create soft-link for fish shell variable(this can define the color of fish shell prompt)."
+
+ls -alh ~/.config/fish
