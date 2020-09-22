fd4413b4aa4bead090209e4610cab478803ea53e
Xun Zhou <segentor@gmail.com>
2020-09-22 21:20:06 +0200

    updated aliases

--- fish/config.fish
+++ fish/config.fish
@@ -6,14 +6,13 @@ source ~/.bash_profile
 # 定义vim 到 mvim
 alias ce='export VISUAL=vim; crontab -e'
 
-# chrome shortcut 
+# jupyter notebook 
 # -------------------------------------------------------------------------------------------------
-alias fo='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --app=http://vertriebsportal.localhost:8080'
-function foo
-  /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --app=$argv
-end
+alias notebook='jupyter notebook'
+alias phpnote='cd /Users/zhoux/sites/jupyter-notebooks/php-notebooks; notebook;'
+alias pythonnote='cd /Users/zhoux/sites/jupyter-notebooks/python-notebooks; notebook;'
 
-# paths github
+# shortcut to github project
 # -------------------------------------------------------------------------------------------------
 alias rolex='cd /Users/zhoux/sites/github/extensions-demo/rolex'
 alias nutab='cd /Users/zhoux/sites/github/extensions-demo/nutab'
@@ -26,42 +25,64 @@ alias aino='cd ~/sites/github/aino; code .'
 alias meiri='cd ~/sites/github/meiri; code .'
 alias life='cd ~/sites/github/lifegrid; code .'
 alias nines='cd ~/sites/github/nines; code .'
+alias pix='cd ~/sites/github/pixss; code .'
 alias pattern='cd ~/sites/github/react-pattern; code .'
 alias minimal='cd ~/sites/github/minimal-react-starter; code .'
 alias rmb='cd ~/sites/github/rmb'
 alias retinder='cd ~/sites/github/retinder'
+alias licai='cd /Users/zhoux/sites/github/licai'
+alias guestbook='cd /Users/zhoux/sites/github/symfony5/guestbook'
 
-# react + snowpack 
-# -------------------------------------------------------------------------------------------------
-function snowreact
-  npx create-snowpack-app $argv --template @snowpack/app-template-react-typescript
-end
-
-
-
-# STACKIT APP CLOUD 
+# Cloud Foundry 
 # -------------------------------------------------------------------------------------------------
 alias cflogin='cf login -a https://api.system.eu03.stackit.schwarz --sso'
-alias cfhelp='cf help -a'
+alias cflo='cf lo'
+alias cfloginextern='cf login -a https://api.system.eu01.cloud.schwarz --sso'
+alias cfdb='cf env carsales | grep mysql:'
+alias db-prod='cf ssh -L 63306:msd1448f7-mysql-0.node.dc1.a9ssvc:3306 carsales'
+alias db-qa='cf ssh -L 63306:msd94aac2-mysql-0.node.dc1.a9ssvc:3306 carsales-qa'
+alias db-staging='cf ssh -L 63306:msd6129ab-mysql-0.node.dc1.a9ssvc:3306 carsales-staging'
+alias db-prod-carex='cf ssh -L 63306:msd752b7c.service.dc1.a9ssvc:3306 carex'
+alias db-qa-carex='cf ssh -L 63306:msd706415.service.dc1.a9ssvc:3306 carex-qa'
 
+# log 
+# -------------------------------------------------------------------------------------------------
+function log
+    if test $argv = 'carex'
+        tail -f ~/lidl/carsales/logs/nginx/error.log ~/lidl/carsales/logs/nginx/app_error.log 
+    else if test $argv = 'error'
+        tail -f  ~/lidl/carsales/logs/app/dev.log | grep "error"
+    else if test $argv = 'inventory'
+        tail -f ~/lidl/carsales/logs/app/dev.log | grep "INVENTORY"
+    else if test $argv = 'sap'
+        tail -f ~/lidl/carsales/logs/app/dev.log | grep 'SAP Import'
+    else if test $argv = 'anm'
+        tail -f ~/lidl/carsales/logs/app/dev.log | grep 'A&M'
+    else if test $argv = 'schwacke'
+        tail -f ~/lidl/carsales/logs/app/dev.log | grep 'Schwacke'
+    else
+        tail -f ~/lidl/carsales/logs/app/dev.log
+    end
+end
 
-# paths SMS
+# carsales 
 # -------------------------------------------------------------------------------------------------
 alias sms='cd ~/lidl/carsales'
-alias ex='cd /Users/zhoux/lidl/carex'
-alias run='sms; bash run.sh '
 alias test:jest='sms; docker-compose run --rm node npm run test '
 alias test:jest:path='sms; docker-compose run --rm node npm test -- --runTestsByPath '
 alias test:unit='sms; bash run.sh test:phpunit '
 alias test:behat='sms; print "🔥 Please [npm run build], if JS files getting updated!  \n\n"; bash run.sh test:behat '
-alias test:behat:c='sms; print "🔥 Please [npm run build], if JS files getting updated!  \n\n"; sms; '
-alias node:download='sms; docker-compose run --rm node npm run download' 
-alias node:test='sms; docker-compose run --rm node npm run test ' 
 alias fe='cd ~/lidl/carsales/app/_frontend'
 alias fd='fe; y devserver'
-alias fb='fe; npm run build'
-alias ff='fe; node_modules/.bin/eslint --fix .'
-alias mjest='fe; majestic --app'
+alias fb='fe; npm run build; cd -'
+alias dc='docker-compose'
+alias dcphp='docker-compose exec php bin/console '
+alias dcworker='sms; docker-compose exec php_worker bin/console '
+alias worker_reset='sms; docker-compose stop php_worker; docker-compose up -d'
+alias dccron='docker-compose exec php_cron bin/console '
+alias bmwimport='dcphp car:import:bmw:xml features/testFiles/bmwImport/BMW_SharePoint_00000583.valid.xml'
+alias t4='dcphp car:export:sap T4'
+alias t5='dcphp car:export:sap T5'
 
 function reset:nginx
     sms; and bash run.sh reset:nginx
@@ -71,47 +92,46 @@ function reset:php
     sms; and bash run.sh reset:php
 end
 
-function reset:db
+function reset:worker
+    sms; 
+    docker-compose stop php_worker
+    docker-compose start php_worker
+end
+
+function clean:db
     sms
     docker-compose run --rm php bin/console doctrine:database:drop --if-exists -n --force
     docker-compose run --rm php bin/console doctrine:database:create --if-not-exists -n
     docker-compose run --rm php bin/console doctrine:migrations:migrate -vvv -n
+end
+function reset:db
+    sms
+    clean:db
     docker-compose run --rm php bin/console doctrine:fixtures:load -n
 end
 function reset
-    sms; reset:php; reset:nginx; reset:db;
+    sms; reset:php; reset:nginx; reset:db; reset:worker; fb;
 end
 
-function hello 
-  sms 
-  docker-compose up -d
-end
-
-function ciao 
-  sms
-  docker-compose stop
-end
 
 # utilities
 # -------------------------------------------------------------------------------------------------
 function localip --description "show local IP of your maschine"
-    netstat -rn | grep 'default' | awk '{print $2}'
+    ifconfig | grep 192.168.178.255 | awk '{print $2}'
 end
 
 function publicip --description "show public IP of your machine"
     curl ifconfig.me
 end
 
-function pkill --description "pkill a process interactively"
-  ps aux | peco | awk '{ print $2 }' | xargs kill
-end
-
 function port --description "show who is listening to the port $argv"
   lsof -n -i4TCP:$argv | grep LISTEN
 end
+
 function print --description "print text with color"
   printf "\033[32m $argv \033[0m"
 end
+
 function wlan --description "wlan off|on"
   networksetup -setairportpower en1 $argv
 end
@@ -120,7 +140,7 @@ end
 # -------------------------------------------------------------------------------------------------
 alias restore_finder="cd ~/Library/Preferences; and  sudo find com.apple.finder.plist* -exec rm {} \;; and  killall Finder"
 
-# COFNIG
+# SSH config 
 # -------------------------------------------------------------------------------------------------
 alias ss='touch ~/.ssh/config; and cat ~/.ssh/config'
 
@@ -133,7 +153,6 @@ alias cr='composer require '
 
 # yarn
 # -------------------------------------------------------------------------------------------------
-alias ns='npm search '
 alias y='yarn'
 alias yi='yarn install'
 alias ys='yarn start'
@@ -147,9 +166,10 @@ alias ni='npm install '
 alias nui='npm uninstall '
 alias nb='npm run build '
 alias ns='npm run start '
+alias ns='npm search '
 alias nd='npm run dev '
 alias nw='npm run watch'
-alias np='npm publish '
+alias np='npm run deploy'
 alias psize='npx package-size '
 alias nlg='npm list -g --depth=0'
 
@@ -158,80 +178,59 @@ alias nlg='npm list -g --depth=0'
 alias bi='brew install'
 alias bs='brew search'
 
-# TOOLs Software
-# -------------------------------------------------------------------------------------------------
-alias pw="pwgen"
-
-
 # system wide
 # -------------------------------------------------------------------------------------------------
 alias ..="cd ..; and  ls"
 alias ...="cd ../..; and  ls"
 alias version='echo nodejs; and  node -v; and  echo npm; and  npm -v; and  php -v; and  docker -v; and  zsh --version'
-alias lk='ls -a | egrep "^\."'
+alias lh='ls -a | egrep "^\."'
 alias l='ls -alh'
 alias h='cd ~'
 alias c='clear'
 alias x="exit"
-alias ap='cd ~/sites/github/awesome-macbook-setup/; and ga .; and gm "update fish aliases"; and git push -f'
+alias ap='cd ~/sites/github/awesome-macbook-setup/; and ga .; and gam "updated aliases"; and git push -f'
 alias au='source ~/sites/github/awesome-macbook-setup/fish/config.fish; echo "fish aliases updated!"'
 alias ae='vi ~/sites/github/awesome-macbook-setup/fish/config.fish'
 alias as='alias | grep '
 alias show="defaults write com.apple.Finder AppleShowAllFiles -bool true; and killall Finder"
 alias hide="defaults write com.apple.Finder AppleShowAllFiles -bool false; and killall Finder"
 alias et="rm -rfv ~/.Trash"
+alias sf='symfony'
+alias pw="pwgen"
 
 # tree
 # -------------------------------------------------------------------------------------------------
 alias t='tree .'
 alias t2='tree -L 2 .'
 alias t3='tree -L 3 .'
+alias t4='tree -L 4 .'
+alias t5='tree -L 5 .'
 
 # Git
 # -------------------------------------------------------------------------------------------------
-alias gt='git tag '
 alias ga='git add '
 alias gap='git add .; and git commit -m "updates"; git push -f'
-alias gam='git add .; and git commit -m "updates"'
-
+alias gam='git add .; and git commit -m '
 alias gp='git push'
-alias gpr='git push -u origin master'
+alias gpr='git push --set-upstream origin master'
 alias gst='git status'
-alias gm='git commit -m'
 alias gca='git commit --amend'
 alias gm-date='git commit --amend --date='
 alias gclean='git clean -f -d'
-alias gri='git rebase -i'
-alias grc='git rebase --continue'
 alias gb='git branch'
 alias gbd='git branch -D'
-alias gbr='git branch -r'
 alias gba='git branch -v -a'
 alias gc='git checkout'
 alias gcb='git checkout -b '
+alias gf='git fetch'
 alias gu='git pull'
 alias gur='git pull --rebase'
-function gcl --description "git clone then enter the directory"
-  git clone $argv;
-  cd $argv; and yarn install; and yarn start;
-end
-#alias gcl='git clone'
-alias gf='git fetch'
 alias gl="git log --graph --decorate"
-alias greset='git reset --hard HEAD'
-alias gtags='git ls-remote --tags'
-alias gs='git stash'
-alias gsl='git stash list'
-alias gss='git stash save '
-alias gsa='git stash apply'
-alias gsp='git stash pop'
+alias gpick='git cherry-pick '
 
 # flutter * dart
 # -------------------------------------------------------------------------------------------------
 alias ios="open -a Simulator"
-function fcreate --description "create flutter project and enter there"
-    flutter create $argv; and f; and cd $argv; and ls -al;
-end
 alias fdevices="flutter devices"
 alias frun="flutter run"
 
@@ -244,7 +243,7 @@ alias vup="vagrant up"
 # -------------------------------------------------------------------------------------------------
 alias dstart='docker-compose start'
 alias dlogs='docker-compose logs -tf'
-alias dup='docker-compose up -d; dlogs'
+alias dup='docker-compose up -d'
 alias ddown="docker-compose down"
 alias dstop='docker-compose stop'
 
@@ -255,27 +254,45 @@ alias dia="docker images -a"
 alias drmi="docker rmi -f"
 alias drm="docker rm -f"
 
-# tig
-# -------------------------------------------------------------------------------------------------
-alias ts='tig status'
-
-# use "cheat" for detect the usage of comand
+# use "cheat" for detect the usage of command
 # -------------------------------------------------------------------------------------------------
 function cheat
   curl cheat.sh/$argv
 end
 
-# PluraSight download
+# download: PluraSight video
 # -------------------------------------------------------------------------------------------------
 function sight
   cd ~/Movies/plura_sight
   youtube-dl --username "xun.zhou@lidl.com" --password "Albert&22912" --verbose --sleep-interval 120 "$argv"
 end
 
-# youtube mp3 download
+# download: youtube to mp3
 # -------------------------------------------------------------------------------------------------
 function yd
   cd ~/Documents/music
   youtube-dl -x --audio-format mp3 "$argv"
+end
+
+# utilities
+# -------------------------------------------------------------------------------------------------
+function localip --description "show local IP of your maschine"
+    ifconfig | grep 192.168.178.255 | awk '{print $2}'
+end
+
+function publicip --description "show public IP of your machine"
+    curl ifconfig.me
+end
+
+function port --description "show who is listening to the port $argv"
+  lsof -n -i4TCP:$argv | grep LISTEN
+end
+
+function print --description "print text with color"
+  printf "\033[32m $argv \033[0m"
+end
+
+function wlan --description "wlan off|on"
+  networksetup -setairportpower en1 $argv
 end
 
